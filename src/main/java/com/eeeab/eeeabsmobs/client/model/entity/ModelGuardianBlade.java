16f56a5d0f2e1484b36054398913dec2f7aa6fa2
EEEAB <2055526030@qq.com>
2023-11-22 15:35:53 +0800

    Revise GuardianBlade Model/Render

--- src/main/java/com/eeeab/eeeabsmobs/client/model/entity/ModelGuardianBlade.java
+++ src/main/java/com/eeeab/eeeabsmobs/client/model/entity/ModelGuardianBlade.java
@@ -8,7 +8,7 @@ import com.github.alexthe666.citadel.client.model.basic.BasicModelPart;
 import com.google.common.collect.ImmutableList;
 
 public class ModelGuardianBlade extends AdvancedEntityModel<EntityGuardianBlade> {
-    private final AdvancedModelBox root;
+    public final AdvancedModelBox root;
     private final AdvancedModelBox upper;
     private final AdvancedModelBox lower;
 
@@ -16,26 +16,26 @@ public class ModelGuardianBlade extends AdvancedEntityModel<EntityGuardianBlade>
         texHeight = 256;
         texWidth = 256;
 
-        root=new AdvancedModelBox(this,"root");
-        root.setPos(0.0F, 24.0F, -13.0F);
-        root.mirror=true;
-        root.setTextureOffset(0,0).addBox(-2.0F, -13.0F, 2.0F, 4.0F, 13.0F, 35.0F)
-                .setTextureOffset(0,180).addBox(0.0F, -13.0F, 37.0F, 0.0F, 13.0F, 35.0F);
+        root = new AdvancedModelBox(this, "root");
+        root.setPos(0.0F, 24.0F, 0F);
+        root.mirror = true;
+        root.setTextureOffset(0, 0).addBox(-2.0F, -13.0F, -11.0F, 4.0F, 13.0F, 35.0F)
+                .setTextureOffset(0, 180).addBox(0.0F, -13.0F, 24.0F, 0.0F, 13.0F, 35.0F);
 
-        upper=new AdvancedModelBox(this,"upper");
-        upper.setPos(0.0F, 0.0F, 13.0F);
+        upper = new AdvancedModelBox(this, "upper");
+        upper.setPos(0.0F, 0.0F, 0F);
         root.addChild(upper);
         upper.setTextureOffset(89, 0).addBox(-1.0F, -34.0F, -11.0F, 2.0F, 21.0F, 24.0F)
                 .setTextureOffset(0, 164).addBox(0.0F, -34.0F, 13.0F, 0.0F, 21.0F, 24.0F)
                 .setTextureOffset(0, 154).addBox(0.0F, -55.0F, -3.0F, 0.0F, 21.0F, 8.0F)
                 .setTextureOffset(0, 0).addBox(-0.5F, -55.0F, -11.0F, 1.0F, 21.0F, 8.0F)
                 .setTextureOffset(0, 107).addBox(0.0F, -63.0F, -11.0F, 0.0F, 8.0F, 42.0F)
-                .setTextureOffset(0, 0).addBox(0-0.5F, -55.0F, -11.0F, 1.0F, 21.0F, 8.0F);
+                .setTextureOffset(0, 0).addBox(-0.5F, -55.0F, -11.0F, 1.0F, 21.0F, 8.0F);
 
-        lower=new AdvancedModelBox(this,"lower");
-        lower.setPos(0.0F, -14.0F, 13.0F);
+        lower = new AdvancedModelBox(this, "lower");
+        lower.setPos(0.0F, -14.0F, 0F);
         root.addChild(lower);
-        lower.mirror=true;
+        lower.mirror = true;
         lower.setTextureOffset(7, 53).addBox(-1.0F, 13.0F, -11.0F, 2.0F, 21.0F, 35.0F)
                 .setTextureOffset(0, 174).addBox(0.0F, 14.0F, 24.0F, 0.0F, 20.0F, 60.0F);
         updateDefaultPose();
@@ -43,7 +43,7 @@ public class ModelGuardianBlade extends AdvancedEntityModel<EntityGuardianBlade>
 
     @Override
     public Iterable<AdvancedModelBox> getAllParts() {
-        return ImmutableList.of(root,upper,lower);
+        return ImmutableList.of(root, upper, lower);
     }
 
     @Override
@@ -53,6 +53,5 @@ public class ModelGuardianBlade extends AdvancedEntityModel<EntityGuardianBlade>
 
     @Override
     public void setupAnim(EntityGuardianBlade entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-
     }
 }
--- src/main/java/com/eeeab/eeeabsmobs/client/render/effects/RenderGuardianBlade.java
+++ src/main/java/com/eeeab/eeeabsmobs/client/render/effects/RenderGuardianBlade.java
@@ -5,6 +5,7 @@ import com.eeeab.eeeabsmobs.client.model.entity.ModelGuardianBlade;
 import com.eeeab.eeeabsmobs.client.render.EERenderType;
 import com.eeeab.eeeabsmobs.sever.entity.impl.effect.EntityGuardianBlade;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
@@ -13,6 +14,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 
+//TODO 渲染偏移问题待解决
 public class RenderGuardianBlade extends EntityRenderer<EntityGuardianBlade> {
     private final ModelGuardianBlade MODEL = new ModelGuardianBlade();
     private final ResourceLocation[] LOCATIONS = new ResourceLocation[6];
@@ -21,19 +23,21 @@ public class RenderGuardianBlade extends EntityRenderer<EntityGuardianBlade> {
         super(pContext);
         //初始化文件地址
         for (int i = 0; i < LOCATIONS.length; i++) {
-            LOCATIONS[i] = new ResourceLocation(EEEABMobs.MOD_ID, "textures/effects/guardian_blade/0" + (i + 1) + ".png");//TODO 待更新纹理
+            LOCATIONS[i] = new ResourceLocation(EEEABMobs.MOD_ID, "textures/effects/guardian_blade/gb_" + (i + 1) + ".png");//TODO 待更新纹理
         }
     }
 
     @Override
     public void render(EntityGuardianBlade blade, float pEntityYaw, float pPartialTick, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight) {
         pPoseStack.pushPose();
-        pPoseStack.translate(0.0F, 1.5F, 0.5F);
+        pPoseStack.translate(0F, 1.5F, 0.5F);
         pPoseStack.scale(1F, -1.0F, -1.0F);
         int timer = blade.alphaControlled.getPrevTimer();
         float alpha = 1.0F - (timer * 0.1F);
+        pPoseStack.mulPose(Axis.YP.rotationDegrees(blade.getYRot()));
         MODEL.renderToBuffer(pPoseStack, pBuffer.getBuffer(EERenderType.getGlowingCutOutEffect(getTextureLocation(blade))), pPackedLight, OverlayTexture.NO_OVERLAY, 1, 1, 1, alpha);
         pPoseStack.popPose();
+        super.render(blade, pEntityYaw, pPartialTick, pPoseStack, pBuffer, pPackedLight);
     }
 
     @Override
