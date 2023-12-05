70436b627c48b6a73ab3b816f2a535b3b88679c6
EEEAB <2055526030@qq.com>
2023-12-05 17:38:07 +0800

    Add-NamelessGuardian strengthen Skills

--- src/main/java/com/eeeab/eeeabsmobs/sever/entity/ai/goal/GuardianLobedAttackGoal.java
+++ src/main/java/com/eeeab/eeeabsmobs/sever/entity/ai/goal/GuardianLobedAttackGoal.java
@@ -1,10 +1,12 @@
 package com.eeeab.eeeabsmobs.sever.entity.ai.goal;
 
 import com.eeeab.eeeabsmobs.sever.entity.impl.effect.EntityCameraShake;
+import com.eeeab.eeeabsmobs.sever.entity.impl.effect.EntityGuardianBlade;
 import com.eeeab.eeeabsmobs.sever.entity.impl.namelessguardian.EntityNamelessGuardian;
 import com.eeeab.eeeabsmobs.sever.entity.ai.goal.animation.base.AnimationCommonGoal;
 import com.github.alexthe666.citadel.animation.Animation;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 
 public class GuardianLobedAttackGoal extends AnimationCommonGoal<EntityNamelessGuardian> {
@@ -26,10 +28,10 @@ public class GuardianLobedAttackGoal extends AnimationCommonGoal<EntityNamelessG
         int maxFallingDistance = isPowered ? 8 : 6;
         int tick = entity.getAnimationTick();
         LivingEntity target = entity.getTarget();
-        //if (target != null) entity.getLookControl().setLookAt(target, 30F, 30F);
         if (target != null) entity.lookAt(target, 30F, 30F);
         if ((!isPowered && tick >= 22 && tick < 34) || (isPowered && tick >= 22 && tick < 40)) {
             if (tick == 23) {
+                if (isPowered) doSpawnBlade();
                 entity.playSound(SoundEvents.GENERIC_EXPLODE, 1.5F, 1F + entity.getRandom().nextFloat() * 0.1F);
                 EntityCameraShake.cameraShake(entity.level(), entity.position(), 20, 0.125F, 5, 20);
             } else if (tick == 24) {
@@ -37,6 +39,17 @@ public class GuardianLobedAttackGoal extends AnimationCommonGoal<EntityNamelessG
             }
             entity.lobedAttack(tick - 13, 2.0, 0.0, maxFallingDistance, isPowered ? 1.25F : 1.0F);
         }
+    }
 
+    private void doSpawnBlade() {
+        final int count = 6;//生成刃的数量
+        for (int i = 0; i < count; ++i) {
+            float f1 = (float) (entity.getYRot() + (float) i * (float) Math.PI * (2.0 / count));
+            double x = entity.getX() + Mth.cos(f1) * 1.5D;
+            double y = entity.getY();
+            double z = entity.getZ() + Mth.sin(f1) * 1.5D;
+            EntityGuardianBlade blade = new EntityGuardianBlade(entity.level(), entity, x, y, z, f1);
+            entity.level().addFreshEntity(blade);
+        }
     }
 }
