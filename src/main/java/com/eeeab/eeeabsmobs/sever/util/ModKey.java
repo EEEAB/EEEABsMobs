edbcc7b969856ae7bbcf4dc49c3513b762d81bb9
EEEAB <2055526030@qq.com>
2023-12-08 17:57:35 +0800

    Update-To v0.4

--- src/main/java/com/eeeab/eeeabsmobs/sever/util/ModKey.java
+++ src/main/java/com/eeeab/eeeabsmobs/sever/util/ModKey.java
@@ -4,13 +4,24 @@ import com.eeeab.eeeabsmobs.EEEABMobs;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.level.levelgen.structure.Structure;
 
 public class ModKey {
-    //结构键
+    //结构资源键
     public static ResourceKey<Structure> ANCIENT_TOMB = registerStructureKey("ancient_tomb");
+
+    //伤害源资源键
+    public static final ResourceKey<DamageType> SHAMAN_BOMBING = registerDamageSourceKey("shaman_bomb");
+    public static final ResourceKey<DamageType> ROBUST_ATTACK = registerDamageSourceKey("guardian_robust_attack");
+    public static final ResourceKey<DamageType> GUARDIAN_LASER = registerDamageSourceKey("guardian_laser_attack");
+
 
     public static ResourceKey<Structure> registerStructureKey(String key) {
         return ResourceKey.create(Registries.STRUCTURE, new ResourceLocation(EEEABMobs.MOD_ID, key));
+    }
+
+    public static ResourceKey<DamageType> registerDamageSourceKey(String key) {
+        return ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation(EEEABMobs.MOD_ID, key));
     }
 }
--- src/main/java/com/eeeab/eeeabsmobs/sever/world/datagen/EEDamageTypeProvider.java
+++ src/main/java/com/eeeab/eeeabsmobs/sever/world/datagen/EEDamageTypeProvider.java
@@ -1,7 +1,7 @@
 package com.eeeab.eeeabsmobs.sever.world.datagen;
 
 import com.eeeab.eeeabsmobs.EEEABMobs;
-import com.eeeab.eeeabsmobs.sever.util.ModDamageSource;
+import com.eeeab.eeeabsmobs.sever.util.ModKey;
 import com.eeeab.eeeabsmobs.sever.util.ModTag;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
@@ -21,9 +21,8 @@ public class EEDamageTypeProvider extends DamageTypeTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
-        //super.addTags(provider);
-        this.tag(DamageTypeTags.IS_PROJECTILE).add(ModDamageSource.SHAMAN_BOMBING);
-        this.tag(DamageTypeTags.BYPASSES_ARMOR).add(ModDamageSource.GUARDIAN_LASER);
+        this.tag(DamageTypeTags.IS_PROJECTILE).add(ModKey.SHAMAN_BOMBING);
+        this.tag(DamageTypeTags.BYPASSES_ARMOR).add(ModKey.GUARDIAN_LASER);
         this.tag(ModTag.GENERAL_UNRESISTANT_TO).add(DamageTypes.FELL_OUT_OF_WORLD).add(DamageTypes.GENERIC_KILL);
         this.tag(ModTag.MAGIC_UNRESISTANT_TO).add(DamageTypes.MAGIC, DamageTypes.INDIRECT_MAGIC, DamageTypes.SONIC_BOOM, DamageTypes.THORNS);
     }
