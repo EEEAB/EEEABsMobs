18a77342e9fe3897aa4531f44454c1ee1c08cfed
EEEAB <2055526030@qq.com>
2024-04-01 11:04:15 +0800

    新增药水效果'狂暴'

---
+++ src/main/java/com/eeeab/eeeabsmobs/sever/potion/EffectFrenzy.java
@@ -0,0 +1,10 @@
+package com.eeeab.eeeabsmobs.sever.potion;
+
+import net.minecraft.world.effect.MobEffectCategory;
+
+//狂暴
+public class EffectFrenzy extends EMEffect {
+    public EffectFrenzy() {
+        super(MobEffectCategory.NEUTRAL, 16711680, false);
+    }
+}
