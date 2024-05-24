79d544939828a3d64f2039b53bd4844c1243d6b3
EEEAB <2055526030@qq.com>
2024-05-24 23:38:15 +0800

    '血球'恢复血量调整

--- src/main/java/com/eeeab/eeeabsmobs/sever/entity/projectile/EntityBloodBall.java
+++ src/main/java/com/eeeab/eeeabsmobs/sever/entity/projectile/EntityBloodBall.java
@@ -2,32 +2,26 @@ package com.eeeab.eeeabsmobs.sever.entity.projectile;
 
 import com.eeeab.eeeabsmobs.client.util.ControlledAnimation;
 import com.eeeab.eeeabsmobs.sever.entity.IEntity;
-import com.eeeab.eeeabsmobs.sever.entity.corpse.EntityCorpseWarlock;
 import com.eeeab.eeeabsmobs.sever.entity.effects.EntityCameraShake;
 import com.eeeab.eeeabsmobs.sever.init.EffectInit;
 import com.eeeab.eeeabsmobs.sever.init.EntityInit;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MoverType;
-import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
-import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
@@ -116,7 +110,7 @@ public class EntityBloodBall extends Projectile implements IEntity {
     private void preDestroy(@Nullable Entity entity) {
         if (!this.level().isClientSide) {
             if (this.getOwner() == entity && entity instanceof LivingEntity livingEntity) {
-                if (this.isHeal) livingEntity.heal(livingEntity.getMaxHealth() * power * 0.1F);
+                if (this.isHeal) livingEntity.heal(Math.max(livingEntity.getMaxHealth() * power * 0.05F, livingEntity.getMaxHealth() * 0.5F));
             } else {
                 this.level().explode(this, this.damageSources().explosion(this, entity), null, this.position(), Math.min(power + 1, 5F), false, Level.ExplosionInteraction.NONE);
                 EntityCameraShake.cameraShake(this.level(), this.position(), 16F, 0.125F, 10, 10);
