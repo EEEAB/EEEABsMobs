ae55670a26f223f9ba51a209475fdbd41682009a
EEEAB <2055526030@qq.com>
2024-06-20 00:16:00 +0800

    幽冥炼狱太刀模型调整

--- src/main/java/com/eeeab/eeeabsmobs/client/model/item/ModelTheNetherworldKatana.java
+++ src/main/java/com/eeeab/eeeabsmobs/client/model/item/ModelTheNetherworldKatana.java
@@ -20,7 +20,9 @@ public class ModelTheNetherworldKatana extends EntityModel<Entity> {
         PartDefinition partdefinition = meshdefinition.getRoot();
         PartDefinition root = partdefinition.addOrReplaceChild("root", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, 20.0F, -1.0F, -1.5708F, 0.0F, 0.0F));
         PartDefinition blade = root.addOrReplaceChild("blade", CubeListBuilder.create().texOffs(0, 0).addBox(-4.5F, -55.4F, -2.5F, 9.0F, 54.0F, 5.0F, new CubeDeformation(-2.0F))
-                .texOffs(52, 17).addBox(-2.5F, -66.4F, 0.0F, 5.0F, 13.0F, 0.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(-4.625F, -58.2F, -2.5F, 9.0F, 7.0F, 5.0F, new CubeDeformation(-2.15F))
+                .texOffs(0, 0).addBox(-4.725F, -60.8F, -2.5F, 9.0F, 7.0F, 5.0F, new CubeDeformation(-2.25F))
+                .texOffs(0, 0).addBox(-4.925F, -63.1F, -2.5F, 9.0F, 7.0F, 5.0F, new CubeDeformation(-2.45F))
                 .texOffs(28, 17).addBox(-5.0F, -25.4F, -2.5F, 7.0F, 24.0F, 5.0F, new CubeDeformation(-1.9F)), PartPose.offsetAndRotation(0.0F, -8.0F, 0.0F, 0.0F, -1.5708F, 0.0F));
         PartDefinition hilt = root.addOrReplaceChild("hilt", CubeListBuilder.create(), PartPose.offset(0.25F, -10.0F, 0.0F));
         PartDefinition cube_r1 = hilt.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(33, 1).addBox(-5.0F, -2.5F, -4.5F, 10.0F, 4.0F, 10.0F, new CubeDeformation(-1.0F)), PartPose.offsetAndRotation(-0.75F, -0.5F, -0.5F, 0.0F, 0.7854F, 0.0F));
