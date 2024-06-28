de807b2ef95d53575afae0f0c6c3173f1a8b244c
EEEAB <2055526030@qq.com>
2024-06-28 22:38:37 +0800

    更新统一掉落经验值

--- src/main/java/com/eeeab/eeeabsmobs/sever/entity/XpReward.java
+++ src/main/java/com/eeeab/eeeabsmobs/sever/entity/XpReward.java
@@ -3,8 +3,10 @@ package com.eeeab.eeeabsmobs.sever.entity;
 public enum XpReward {
     XP_REWARD_NONE(0),
     XP_REWARD_NORMAL(10),
-    XP_REWARD_ELITE(50),
-    XP_REWARD_BOSS(100);
+    XP_REWARD_HARD(50),
+    XP_REWARD_ELITE(100),
+    XP_REWARD_BOSS(200),
+    XP_REWARD_EPIC_BOSS(500);
 
     private final int xp;
 
