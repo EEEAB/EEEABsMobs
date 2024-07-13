45f930153d8216c1232f9c17dacdf4b44f50520d
EEEAB <2055526030@qq.com>
2024-07-13 11:02:05 +0800

    新增异教徒心脏文本描述

---
+++ src/main/java/com/eeeab/eeeabsmobs/sever/item/ItemHeartOfPagan.java
@@ -0,0 +1,22 @@
+package com.eeeab.eeeabsmobs.sever.item;
+
+import com.eeeab.eeeabsmobs.sever.util.EMTUtils;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+
+import java.util.List;
+
+public class ItemHeartOfPagan extends Item {
+    public ItemHeartOfPagan(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @org.jetbrains.annotations.Nullable Level level, List<Component> tooltip, TooltipFlag flagIn) {
+        super.appendHoverText(stack, level, tooltip, flagIn);
+        tooltip.add(EMTUtils.simpleItemText(this.getDescriptionId(), EMTUtils.STYLE_GRAY));
+    }
+}
