becb168005043880765859ea880799203d805faf
EEEAB <2055526030@qq.com>
2024-08-14 14:34:19 +0800

    更新模组版本至0.95

--- src/main/java/com/eeeab/eeeabsmobs/client/particle/util/AdvancedParticleBase.java
+++ src/main/java/com/eeeab/eeeabsmobs/client/particle/util/AdvancedParticleBase.java
@@ -1,6 +1,7 @@
 package com.eeeab.eeeabsmobs.client.particle.util;
 
 import com.eeeab.eeeabsmobs.client.render.EMRenderType;
+import com.eeeab.eeeabsmobs.sever.util.QuaternionUtils;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
@@ -186,13 +187,9 @@ public class AdvancedParticleBase extends TextureSheetParticle {
             float rotX = eulerRot.prevPitch + (eulerRot.pitch - eulerRot.prevPitch) * partialTicks;
             float rotY = eulerRot.prevYaw + (eulerRot.yaw - eulerRot.prevYaw) * partialTicks;
             float rotZ = eulerRot.prevRoll + (eulerRot.roll - eulerRot.prevRoll) * partialTicks;
-            Quaternionf quaternionf = new Quaternionf();
-            //Quaternion quatX = new Quaternion(rotX, 0, 0, false);
-            //Quaternion quatY = new Quaternion(0, rotY, 0, false);
-            //Quaternion quatZ = new Quaternion(0, 0, rotZ, false);
-            Quaternionf quatX = new Quaternionf(quaternionf.rotateZYX(rotX, 0, 0));
-            Quaternionf quatY = new Quaternionf(quaternionf.rotateZYX(0, rotY, 0));
-            Quaternionf quatZ = new Quaternionf(quaternionf.rotateZYX(0, 0, rotZ));
+            Quaternionf quatX = QuaternionUtils.rotationXYZ(rotX, 0, 0, false);
+            Quaternionf quatY = QuaternionUtils.rotationXYZ(0, rotY, 0, false);
+            Quaternionf quatZ = QuaternionUtils.rotationXYZ(0, 0, rotZ, false);
             quaternion.mul(quatZ);
             quaternion.mul(quatY);
             quaternion.mul(quatX);
@@ -203,11 +200,8 @@ public class AdvancedParticleBase extends TextureSheetParticle {
             double z = orientRot.prevOrientation.z + (orientRot.orientation.z - orientRot.prevOrientation.z) * partialTicks;
             float pitch = (float) Math.asin(-y);
             float yaw = (float) (Mth.atan2(x, z));
-            Quaternionf quaternionf = new Quaternionf();
-            //Quaternion quatX = new Quaternion(pitch, 0, 0, false);
-            //Quaternion quatY = new Quaternion(0, yaw, 0, false);
-            Quaternionf quatX = new Quaternionf(quaternionf.rotateZYX(pitch, 0, 0));
-            Quaternionf quatY = new Quaternionf(quaternionf.rotateZYX(0, yaw, 0));
+            Quaternionf quatX = QuaternionUtils.rotationXYZ(pitch, 0, 0, false);
+            Quaternionf quatY = QuaternionUtils.rotationXYZ(0, yaw, 0, false);
             quaternion.mul(quatY);
             quaternion.mul(quatX);
         }
