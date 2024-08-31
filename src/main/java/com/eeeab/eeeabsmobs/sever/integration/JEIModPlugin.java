ef39c6c0066c940f274be1a8d11dbcca7d41db3e
EEEAB <2055526030@qq.com>
2024-08-31 18:35:38 +0800

    新增JEI的部分物品描述以及新增了药水效果的描述

--- src/main/java/com/eeeab/eeeabsmobs/sever/integration/JEIModPlugin.java
+++ src/main/java/com/eeeab/eeeabsmobs/sever/integration/JEIModPlugin.java
@@ -1,21 +1,30 @@
 package com.eeeab.eeeabsmobs.sever.integration;
 
 import com.eeeab.eeeabsmobs.EEEABMobs;
+import com.eeeab.eeeabsmobs.sever.init.ItemInit;
+import com.eeeab.eeeabsmobs.sever.util.EMTUtils;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.registration.IRecipeRegistration;
 import net.minecraft.resources.ResourceLocation;
 
+import java.util.List;
+
 @JeiPlugin
 public class JEIModPlugin implements IModPlugin {
     @Override
     public ResourceLocation getPluginUid() {
-        return new ResourceLocation(EEEABMobs.MOD_ID,"jei_plugin");
+        return new ResourceLocation(EEEABMobs.MOD_ID, "jei_plugin");
     }
 
 
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
         IModPlugin.super.registerRecipes(registration);
+        //物品描述
+        registration.addItemStackInfo(List.of(
+                        ItemInit.ANCIENT_TOMB_EYE.get().getDefaultInstance(),
+                        ItemInit.BLOODY_ALTAR_EYE.get().getDefaultInstance()
+                ), EMTUtils.simpleText("jei.", "eye_of_structure", null));
     }
 }
