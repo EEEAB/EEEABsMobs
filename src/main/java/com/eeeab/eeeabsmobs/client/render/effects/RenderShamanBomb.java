0f2bca8efce4fad5fd609c46b0538159053720bc
EEEAB <2055526030@qq.com>
2024-09-08 10:51:47 +0800

    修正巫师炸弹绘制四边形时过大的问题

--- src/main/java/com/eeeab/eeeabsmobs/client/render/effects/RenderShamanBomb.java
+++ src/main/java/com/eeeab/eeeabsmobs/client/render/effects/RenderShamanBomb.java
@@ -9,12 +9,15 @@ import com.mojang.math.Axis;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import org.joml.Matrix3f;
+import org.joml.Matrix4f;
 
 @OnlyIn(Dist.CLIENT)
 public class RenderShamanBomb extends FlatTextureRenderer<EntityShamanBomb> {
@@ -55,5 +58,18 @@ public class RenderShamanBomb extends FlatTextureRenderer<EntityShamanBomb> {
         VertexConsumer vertexconsumer = buffer.getBuffer(RenderType.entityCutoutNoCull(this.getTextureLocation(entity)));
         renderFlatQuad(matrixStack, vertexconsumer, packedLight, 1F, 0F, 0F, 1F, 1F, 1F, Quad.XY);
         matrixStack.popPose();
+    }
+
+    @Override
+    protected void drawVertex(Matrix4f matrix, Matrix3f normals, VertexConsumer vertexBuilder, float offsetX, float offsetY, float offsetZ, float textureX, float textureY, float alpha, int packedLightIn) {
+        offsetX = offsetX < 0 ? 0 : offsetX;
+        offsetY = offsetY < 0 ? 0 : offsetY;
+        vertexBuilder.vertex(matrix, offsetX - 0.5F, offsetY - 0.25F, offsetZ)
+                .color(1F, 1F, 1F, alpha)
+                .uv(textureX, textureY)
+                .overlayCoords(OverlayTexture.NO_OVERLAY)
+                .uv2(packedLightIn)
+                .normal(normals, 0F, 1F, 0F)
+                .endVertex();
     }
 }
