357f5ff19f9090c540b5650fd261e82f6bd23d89
EEEAB <2055526030@qq.com>
2024-10-14 13:16:26 +0800

    新增通用Boss脱战机制相关接口

--- src/main/java/com/eeeab/eeeabsmobs/sever/entity/IBoss.java
+++ src/main/java/com/eeeab/eeeabsmobs/sever/entity/IBoss.java
@@ -1,23 +1,61 @@
 package com.eeeab.eeeabsmobs.sever.entity;
 
 import com.eeeab.eeeabsmobs.sever.init.SoundInit;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
 
 public interface IBoss extends IEntity {
+    String STRING_ILLEGALITY_COUNT = "illegalityCount";
+
+    int getIllegalityCount();
+
+    void setIllegalityCount(int count);
+
+    default int getMaxIllegalityCount() {
+        return 10;
+    }
 
     /**
-     * @return 检查玩家攻击是否无效
+     * 当非法攻击次数达到设定的最大值时 应让Boss脱离战斗并返回出生点
+     *
+     * @return 是否可以脱战
      */
-    default boolean checkPlayerAttackLegality(Player player, Mob boss, double height) {
-        if (player.isSpectator() || player.isCreative()) return false;
+    default boolean outOfCombatFlag() {
+        return getIllegalityCount() >= getMaxIllegalityCount();
+    }
+
+    /**
+     * 非法攻击次数统计
+     *
+     * @param flag 条件为ture则增长次数反之减少
+     */
+    default void changeIllegalityCount(boolean flag) {
+        if (outOfCombatFlag()) return;
+        setIllegalityCount(Mth.clamp(getIllegalityCount() + (flag ? 1 : -1), 0, getMaxIllegalityCount()));
+    }
+
+    default void addBossSaveData(CompoundTag nbt) {
+        nbt.putInt(STRING_ILLEGALITY_COUNT, this.getIllegalityCount());
+    }
+
+    default void readBossSaveData(CompoundTag nbt) {
+        setIllegalityCount(nbt.getInt(STRING_ILLEGALITY_COUNT));
+    }
+
+    /**
+     * 检查玩家攻击是否合法
+     */
+    default void checkPlayerAttackLegality(Player player, Mob boss, double height) {
+        if (player.isSpectator() || player.isCreative()) return;
         boolean flag = false;
         if (boss.getSensing().hasLineOfSight(player)) {
             double pY = player.getY();
             double bY = boss.getY();
-            flag = player.isFallFlying() || Math.abs(pY - bY) <= height;
+            flag = player.isFallFlying() /* 玩家在使用鞘翅飞行时不做Y轴判断 */ || Math.abs(pY - bY) <= height;
         }
-        if (!flag) boss.playSound(SoundInit.UNDAMAGED.get(), 0.4F, 2F);
-        return !flag;
+        if (!flag) boss.playSound(SoundInit.UNDAMAGED.get(), 1F, 2F);
+        changeIllegalityCount(!flag);
     }
 }
