1e84efef98b64ee31c44229112c0ac3fcaada41b
EEEAB <2055526030@qq.com>
2024-12-27 23:36:29 +0800

    更新翻译字段工具类:简化与规范键名命名规则

--- src/main/java/com/eeeab/eeeabsmobs/sever/util/EMTUtils.java
+++ src/main/java/com/eeeab/eeeabsmobs/sever/util/EMTUtils.java
@@ -4,8 +4,6 @@ import com.eeeab.eeeabsmobs.EEEABMobs;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.network.chat.Style;
-import net.minecraft.network.chat.TextColor;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
@@ -15,21 +13,12 @@ import java.util.List;
  * 翻译字段工具类
  *
  * @author EEEAB
- * @version 1.4
+ * @version 1.5
  */
 public class EMTUtils {
 
     private EMTUtils() {
     }
-
-    //text style
-    public static final Style STYLE_BLUE = Style.EMPTY.withColor(TextColor.fromLegacyFormat(ChatFormatting.BLUE));
-
-    public static final Style STYLE_GRAY = Style.EMPTY.withColor(TextColor.fromLegacyFormat(ChatFormatting.GRAY));
-
-    public static final Style STYLE_GREEN = Style.EMPTY.withColor(TextColor.fromLegacyFormat(ChatFormatting.GREEN));
-
-    public static final Style STYLE_RED = Style.EMPTY.withColor(TextColor.fromLegacyFormat(ChatFormatting.RED));
 
     //general
     public static final String TIP_SUFFIX = ".tip";
@@ -40,8 +29,6 @@ public class EMTUtils {
 
     public static final String MOD_ID = EEEABMobs.MOD_ID + ".";
 
-    public static final Component UNABLE_BREAKS = simpleText(OTHER_PREFIX, "unable_breaks", STYLE_BLUE);//无法破坏
-
     //item
     public static final String ITEM_PREFIX = "item.";
 
@@ -49,66 +36,53 @@ public class EMTUtils {
 
     public static final String ITEM_OTHER_PREFIX = ITEM_PREFIX + OTHER_PREFIX;
 
-    //keyboard Operation
-    public static final String KEY_PREFIX = "keyboard.";
-
-    public static final String SHIFT_DOWN = KEY_PREFIX + "shift_down.";
-
-    public static final String RIGHT_CLICK = KEY_PREFIX + "right_click.";
-
-    public static final String LEFT_CLICK = KEY_PREFIX + "left_click.";
+    public static final String ITEM_STRUCTURE_PREFIX = ITEM_PREFIX + "structure.";
 
     //other
-    public static final String STRUCTURE_PREFIX = "structure.";
+    public static final String KEY_PREFIX = "hotkeys.";
 
-    public static Component simpleText(String prefix, String key, Style style, @Nullable Object... args) {
-        return simpleText(prefix, key, style, TIP_SUFFIX, args);
+    //无法破坏
+    public static final Component UNABLE_BREAKS = simpleText(ITEM_OTHER_PREFIX, "unable_breaks", ChatFormatting.BLUE);
+
+    //按住Shift
+    public static final Component HOLD_SHIFT_KEY = simpleText(ITEM_PREFIX + KEY_PREFIX, "shift_down", ChatFormatting.GREEN);
+
+    public static Component simpleText(String prefix, String key, ChatFormatting format, @Nullable Object... args) {
+        return simpleText(prefix, key, format, TIP_SUFFIX, args);
     }
 
-    public static Component simpleText(String prefix, String key, Style style, String suffix, @Nullable Object... args) {
+    public static Component simpleText(String prefix, String key, ChatFormatting format, String suffix, @Nullable Object... args) {
         MutableComponent component;
         String finalPrefix = prefix + MOD_ID;
-        if (style == null) style = Style.EMPTY;
         if (args == null) args = new Object[0];
         if (key == null) {
-            component = Component.translatable(finalPrefix + suffix.substring(1), args).setStyle(style);
+            component = Component.translatable(finalPrefix + suffix.substring(1), args);
         } else {
             key = subDescriptionId(key);
-            component = Component.translatable(finalPrefix + key + suffix, args).setStyle(style);
+            component = Component.translatable(finalPrefix + key + suffix, args);
         }
+        if (format != null) component.withStyle(format);
         return component;
     }
 
     public static Component itemCoolTime(double args) {
-        return simpleText(ITEM_OTHER_PREFIX, args == 0 ? "no_cd" : "cd", STYLE_GREEN, args);
+        return simpleText(ITEM_OTHER_PREFIX, args == 0 ? "no_cd" : "cd", ChatFormatting.GREEN, args);
     }
 
-    public static Component simpleItemText(String key, Style style, @Nullable Object... args) {
-        return simpleText(ITEM_PREFIX, key, style, args);
+    public static Component simpleItemText(String key, @Nullable Object... args) {
+        return simpleText(ITEM_PREFIX, key, ChatFormatting.GRAY, args);
     }
 
-    public static Component simpleArmorText(String key, Style style, @Nullable Object... args) {
-        return simpleText(ITEM_ARMOR_PREFIX, key, style, args);
+    public static Component simpleArmorText(String key, ChatFormatting format, @Nullable Object... args) {
+        return simpleText(ITEM_ARMOR_PREFIX, key, format, args);
     }
 
-    public static Component simpleOtherText(String key, Style style, @Nullable Object... args) {
-        return simpleText(ITEM_OTHER_PREFIX, key, style, args);
+    public static Component simpleOtherText(String key, ChatFormatting format, @Nullable Object... args) {
+        return simpleText(ITEM_OTHER_PREFIX, key, format, args);
     }
 
-    public static Component simpleShiftDownText(String key, Style style, @Nullable Object... args) {
-        return simpleText(SHIFT_DOWN, key, style, args);
-    }
-
-    public static Component simpleRightClickText(String key, Style style, @Nullable Object... args) {
-        return simpleText(RIGHT_CLICK, key, style, args);
-    }
-
-    public static Component simpleLeftClickText(String key, Style style, @Nullable Object... args) {
-        return simpleText(LEFT_CLICK, key, style, args);
-    }
-
-    public static Component simpleConfigText(String key, Style style, @Nullable Object... args) {
-        return simpleText(OTHER_PREFIX, key, style, CONFIG_SUFFIX, args);
+    public static Component simpleConfigText(String key, ChatFormatting format, @Nullable Object... args) {
+        return simpleText(OTHER_PREFIX, key, format, CONFIG_SUFFIX, args);
     }
 
     /**
@@ -119,13 +93,12 @@ public class EMTUtils {
      * prefix.modId.descriptionId.tip_n<br>
      * ...
      */
-    public static List<Component> complexText(String prefix, int count, Style style, String key, @Nullable Object... args) {
+    public static List<Component> complexText(String prefix, int count, ChatFormatting format, String key, @Nullable Object... args) {
         List<Component> components = new ArrayList<>();
-        if (style == null) style = Style.EMPTY;
         if (args == null) args = new Object[0];
         for (int i = 1; i <= count; i++) {
             StringBuilder sb = new StringBuilder();
-            components.add(Component.translatable(sb
+            MutableComponent component = Component.translatable(sb
                             .append(prefix)
                             .append(MOD_ID)
                             .append(subDescriptionId(key))
@@ -133,7 +106,9 @@ public class EMTUtils {
                             .append("_")
                             .append(i)
                             .toString()
-                    , args).setStyle(style));
+                    , args);
+            if (format != null) component.withStyle(format);
+            components.add(component);
         }
         return components;
     }
@@ -145,9 +120,8 @@ public class EMTUtils {
      * prefix.modId.BBB.tip<br>
      * ...
      */
-    public static List<Component> complexText(String prefix, boolean hasIndex, Style style, String... keys) {
+    public static List<Component> complexText(String prefix, boolean hasIndex, ChatFormatting format, String... keys) {
         List<Component> components = new ArrayList<>();
-        if (style == null) style = Style.EMPTY;
         for (int i = 0; i < keys.length; i++) {
             StringBuilder sb = new StringBuilder();
             sb.append(prefix)
@@ -155,7 +129,9 @@ public class EMTUtils {
                     .append(subDescriptionId(keys[i]))
                     .append(TIP_SUFFIX);
             if (hasIndex) sb.append("_").append(i + 1);
-            components.add(Component.translatable(sb.toString()).setStyle(style));
+            MutableComponent component = Component.translatable(sb.toString());
+            if (format != null) component.withStyle(format);
+            components.add(component);
         }
         return components;
     }
