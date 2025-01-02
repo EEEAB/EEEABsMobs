e113b3fbfaff163faca675e7ec221ff0e4d023f6
EEEAB <2055526030@qq.com>
2025-01-02 13:15:19 +0800

    无名守卫者发光纹理相关调整

--- src/main/java/com/eeeab/eeeabsmobs/client/render/layer/LayerGuardianCore.java
+++ src/main/java/com/eeeab/eeeabsmobs/client/render/layer/LayerGuardianCore.java
@@ -10,7 +10,7 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class LayerGuardianCore extends LayerGlow<EntityNamelessGuardian, ModelNamelessGuardian> {
-    private static final ResourceLocation GLOW_LAYER = new ResourceLocation(EEEABMobs.MOD_ID, "textures/entity/guling/nameless_guardian/nameless_guardian_core.png");
+    private static final ResourceLocation GLOW_LAYER = new ResourceLocation(EEEABMobs.MOD_ID, "textures/entity/guling/nameless_guardian/nameless_guardian_glow.png");
 
     public LayerGuardianCore(RenderLayerParent<EntityNamelessGuardian, ModelNamelessGuardian> renderLayerParent) {
         super(renderLayerParent, GLOW_LAYER, 0.8F, guardian -> !guardian.coreControlled.isStop());
