c67a88371125594c647b7db2dc888d80cb84dff9
EEEAB <2055526030@qq.com>
2026-04-22 23:08:09 +0800

    修复物品减少耐久时动画异常问题

--- src/main/java/com/eeeab/animate/client/util/ItemAnimationUtils.java
+++ src/main/java/com/eeeab/animate/client/util/ItemAnimationUtils.java
@@ -3,57 +3,91 @@ package com.eeeab.animate.client.util;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.Mth;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * 物品动画工具类
  *
  * @author EEEAB
  */
+@OnlyIn(Dist.CLIENT)
 public class ItemAnimationUtils {
-    private static final String ACCUMULATED_TIME_KEY = "accumulatedTime";
-    private static final String ANIMATION_INDEX = "animationIndex";
-    private static final String LAST_TIME_KEY = "lastTime";
+    private static final Map<UUID, AnimationState> ANIMATIONS = new HashMap<>();
+    private static final String ANIMATION_UUID_KEY = "ClientAnimationUUID";
 
+    private static class AnimationState {
+        long lastTimeMs;
+        long accumulatedTimeMs;
+        int animationIndex;
+        boolean started;
+    }
 
-    public static CompoundTag start(ItemStack stack, int tickCount) {
+    private static UUID getOrCreateAnimationUUID(ItemStack stack) {
         CompoundTag tag = stack.getOrCreateTag();
-        tag.putLong(LAST_TIME_KEY, (long) tickCount * 1000L / 20L);
-        tag.putLong(ACCUMULATED_TIME_KEY, 0L);
-        return tag;
+        if (tag.hasUUID(ANIMATION_UUID_KEY)) {
+            return tag.getUUID(ANIMATION_UUID_KEY);
+        } else {
+            UUID uuid = UUID.randomUUID();
+            tag.putUUID(ANIMATION_UUID_KEY, uuid);
+            return uuid;
+        }
+    }
+
+    public static void start(ItemStack stack, int tickCount) {
+        UUID uuid = getOrCreateAnimationUUID(stack);
+        AnimationState state = new AnimationState();
+        state.lastTimeMs = (long) tickCount * 1000L / 20L;
+        state.accumulatedTimeMs = 0L;
+        state.animationIndex = 0;
+        state.started = true;
+        ANIMATIONS.put(uuid, state);
     }
 
     public static void start(ItemStack stack, int tickCount, int index) {
-        start(stack, tickCount).putInt(ANIMATION_INDEX, index);
+        start(stack, tickCount);
+        UUID uuid = getOrCreateAnimationUUID(stack);
+        AnimationState state = ANIMATIONS.get(uuid);
+        if (state != null) state.animationIndex = index;
     }
 
     public static boolean updateTime(ItemStack stack, float ageInTicks, float speed) {
-        boolean flag = isStarted(stack);
-        if (flag) {
-            CompoundTag tag = stack.getOrCreateTag();
-            long i = Mth.lfloor(ageInTicks * 1000.0F / 20.0F);
-            long lastTime = tag.getLong(LAST_TIME_KEY);
-            tag.putLong(ACCUMULATED_TIME_KEY, tag.getLong(ACCUMULATED_TIME_KEY) + (long) ((float) (i - lastTime) * speed));
-            tag.putLong(LAST_TIME_KEY, i);
+        UUID uuid = getOrCreateAnimationUUID(stack);
+        AnimationState state = ANIMATIONS.get(uuid);
+        if (state == null || !state.started) return false;
+        long currentMs = Mth.lfloor(ageInTicks * 1000.0F / 20.0F);
+        long delta = currentMs - state.lastTimeMs;
+        if (delta > 0) {
+            state.accumulatedTimeMs += (long) (delta * speed);
+            state.lastTimeMs = currentMs;
         }
-        return flag;
+        return true;
     }
 
     public static void stop(ItemStack stack) {
-        stack.getOrCreateTag().putLong(LAST_TIME_KEY, Long.MAX_VALUE);
+        UUID uuid = getOrCreateAnimationUUID(stack);
+        ANIMATIONS.remove(uuid);
     }
 
     public static long getAccumulatedTime(ItemStack stack) {
-        CompoundTag tag = stack.getTag();
-        return tag != null ? tag.getLong(ACCUMULATED_TIME_KEY) : 0L;
+        UUID uuid = getOrCreateAnimationUUID(stack);
+        AnimationState state = ANIMATIONS.get(uuid);
+        return state != null ? state.accumulatedTimeMs : 0L;
     }
 
     public static int getAnimationIndex(ItemStack stack) {
-        CompoundTag tag = stack.getTag();
-        return tag != null ? tag.getInt(ANIMATION_INDEX) : 0;
+        UUID uuid = getOrCreateAnimationUUID(stack);
+        AnimationState state = ANIMATIONS.get(uuid);
+        return state != null ? state.animationIndex : 0;
     }
 
     public static boolean isStarted(ItemStack stack) {
-        CompoundTag tag = stack.getTag();
-        return tag != null && tag.getLong(LAST_TIME_KEY) != Long.MAX_VALUE;
+        UUID uuid = getOrCreateAnimationUUID(stack);
+        AnimationState state = ANIMATIONS.get(uuid);
+        return state != null && state.started;
     }
 }
